cdc997790e9efb480b7645e14ec1d232bc6b0300
merak <MarekBillington@users.noreply.github.com>
2023-07-22 17:26:16 +0100

    HTMX + Go test

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
+
+func main() {
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		// io.WriteString(w, "hello world")
+		// io.WriteString(w, r.Method)
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Barbie", Director: "Greta Gerwig"},
+				{Title: "Oppenheimer", Director: "Christopher Nolan"},
+			},
+		}
+
+		tmpl.Execute(w, films)
+	}
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second)
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-elements", Film{Title: title, Director: director})
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
+
+	fmt.Println("Open to port 8000")
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
